40b7fab6f5c14c890cf43b7f9bec89d06fead662
ryanmgrum <69282553+ryanmgrum@users.noreply.github.com>
2021-12-24 11:09:16 -0600

    Changed Spark's logging level to ERROR from WARN.

--- src/main/scala/HiveConnection.scala
+++ src/main/scala/HiveConnection.scala
@@ -5,8 +5,8 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 class HiveConnection {
 
   protected def connect() : SparkSession = {
-    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
-    Logger.getLogger("org.spark-project").setLevel(Level.WARN)
+    Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
+    Logger.getLogger("org.spark-project").setLevel(Level.ERROR)
     System.setProperty("hadoop.home.dir", "C:\\hadoop")
     val spark = SparkSession
       .builder
@@ -14,7 +14,7 @@ class HiveConnection {
       .config("spark.master", "local")
       .enableHiveSupport()
       .getOrCreate();
-    spark.sparkContext.setLogLevel("WARN")
+    spark.sparkContext.setLogLevel("ERROR")
     return spark;
   }
 
