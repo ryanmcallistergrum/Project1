ba26a3f3e2eb40a8ed349c904b30142243225cd7
ryanmgrum <69282553+ryanmgrum@users.noreply.github.com>
2021-12-24 11:50:23 -0600

    Changed the Class file from having a Companion Object to just a regular Class.

--- src/main/scala/HiveConnection.scala
+++ src/main/scala/HiveConnection.scala
@@ -34,36 +34,3 @@ class HiveConnection {
     spark.close();
   }
 }
-
-object HiveConnection {
-
-  protected def connect() : SparkSession = {
-    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
-    Logger.getLogger("org.spark-project").setLevel(Level.WARN)
-    System.setProperty("hadoop.home.dir", "C:\\hadoop")
-    val spark = SparkSession
-      .builder
-      .appName("Project1")
-      .config("spark.master", "local")
-      .enableHiveSupport()
-      .getOrCreate();
-    spark.sparkContext.setLogLevel("WARN")
-    return spark;
-  }
-
-  protected def executeDML(spark : SparkSession, sql : String) : Unit = {
-    spark.sql(sql).queryExecution;
-  }
-
-  protected def executeQuery(spark : SparkSession, sql : String) : DataFrame = {
-    return spark.sql(sql);
-  }
-
-  protected def showQuery(spark : SparkSession, sql : String) : Unit = {
-    spark.sql(sql).show();
-  }
-
-  protected def disconnect(spark : SparkSession) : Unit = {
-    spark.close();
-  }
-}
