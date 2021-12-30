4c2519b2a49f4b360cdfc562646872668f746e52
ryanmgrum <69282553+ryanmgrum@users.noreply.github.com>
2021-12-30 12:46:53 -0600

    In BasicController, BasicView, AdminController, and AdminView: Renamed the concept of Main Menu to User Menu, which also means renaming the mainMenu methods to userMenu.

--- src/main/scala/BasicView.scala
+++ src/main/scala/BasicView.scala
@@ -1,6 +1,6 @@
 object BasicView {
-  def mainMenu() : Unit = {
-    println("Welcome to the user menu. Please select from one of the following options:");
+  def userMenu() : Unit = {
+    println("Welcome to the User Menu. Please select from one of the following options:");
     println("1. Return to Home Screen");
     println("2. Execute Query");
     println("3. Change Username");
--- src/test/scala/TestBasicView.scala
+++ src/test/scala/TestBasicView.scala
@@ -2,7 +2,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class TestBasicView  extends AnyFlatSpec with should.Matchers {
-  "mainMenu()" should "display the Main Menu options for BASIC users" in {
-    BasicView.mainMenu();
+  "userMenu()" should "display the User Menu options for BASIC users" in {
+    BasicView.userMenu();
   }
 }
