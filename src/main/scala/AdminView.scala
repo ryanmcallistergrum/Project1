fc4df1f41715ea35c656bf62ac665bce7fc1f568
ryanmgrum <69282553+ryanmgrum@users.noreply.github.com>
2021-12-30 22:18:28 -0600

    Initial commit.

---
+++ src/main/scala/AdminView.scala
@@ -0,0 +1,26 @@
+object AdminView {
+  def userMenu() : Unit = {
+    println("Welcome to the User Menu. Please select from one of the following options:");
+    println("1. Return to Home Screen");
+    println("2. Manage Queries");
+    println("3. Manage Users");
+    println("4. Change Username");
+    println("5. Change Password");
+    println("6. Log Out");
+  }
+
+  def manageQueries() : Unit = {
+    println("Welcome to the Query Management Menu. Please select from one of the following options:");
+    println("1. Execute Query");
+    println("2. Rename Query");
+    println("3. Write Query");
+    println("4. Delete Query");
+  }
+
+  def manageUsers() : Unit = {
+    println("Welcome to the User Management Menu. Please select from one of the following options:");
+    println("1. View Users");
+    println("2. Delete User");
+    println("3. Promote Basic User to Admin");
+  }
+}
