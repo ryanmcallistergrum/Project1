1db350edc546d25cdfc3fd2ceff32cc67e3ee3c6
ryanmgrum <69282553+ryanmgrum@users.noreply.github.com>
2021-12-31 14:32:28 -0600

    Removed extra space in test description.

--- src/test/scala/TestBasicController.scala
+++ src/test/scala/TestBasicController.scala
@@ -1,7 +1,7 @@
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
-class TestBasicController  extends AnyFlatSpec with should.Matchers {
+class TestBasicController extends AnyFlatSpec with should.Matchers {
   "authorize(String, String)" should "bring the user into the BASIC user's Home Screen, where they can see the database updates from the API" in {
     if (!HiveDBManager.usernameExists("basic"))
       HiveDBManager.addUser("basic", "basic");
